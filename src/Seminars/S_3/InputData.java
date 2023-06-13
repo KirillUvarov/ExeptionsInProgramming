cdff696c1aaec5b5e30d83cfc69c72dd3b6ba345
KirillUvarov <112546441+KirillUvarov@users.noreply.github.com>
2023-06-13 22:35:30 +0300

    HW_S3

---
+++ src/Seminars/S_3/InputData.java
@@ -0,0 +1,20 @@
+package Seminars.S_3;
+
+import java.util.Scanner;
+
+public class InputData {
+    public String[] enterData() {
+        Scanner iScanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Введите строку через ПРОБЕЛ (ФИО, д.р, тел., пол) Формата: Фамилия Имя Отчество  dd.mm.yyyy 000111222111 f: ");
+            String data = iScanner.nextLine();
+            String[] arrayData = data.split(" ");
+            if (arrayData.length == 6) {
+                return arrayData;
+            } else if (arrayData.length < 6){
+                System.out.println("you have not entered all the data, please try again");
+            } else System.out.println("you entered more data, please try again");
+        }
+
+    }
+}
